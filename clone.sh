ea1407211185b918188076081465c598112daad3
perillamint <perillamint@quendi.moe>
2018-04-26 15:05:09 +0900

    SHA256 verification for tegra_mtc.bin

--- clone.sh
+++ clone.sh
@@ -25,5 +25,9 @@ git reset --hard 0a322b01cacf03e3be727e3e4c3d46d69f2e343e
 cd ..
 
 cd coreboot
+set -e
 curl https://gist.githubusercontent.com/perillamint/481beca39ba82ff45dbe1b2dca86c673/raw/8cf20a067ba38e1e1cb9adf64f4bd28b440be3eb/tegra_mtc.bin.b64 | base64 -d > tegra_mtc.bin
+# You can verify this hash on https://fail0verflow.com/blog/2018/shofel2/
+echo "edb32e3f9ed15b55e780e8a01ef927a3b8a1f25b34a6f95467041d8953777d21 tegra_mtc.bin" | sha256sum -c -
+set +e
 cd ..
