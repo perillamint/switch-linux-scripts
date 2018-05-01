ea90652879ebcb605bbae6423144434d30a2cd9d
perillamint <perillamint@quendi.moe>
2018-05-02 01:21:18 +0900

    Fix for busybox sha256sum

--- clone.sh
+++ clone.sh
@@ -13,6 +13,6 @@ cd coreboot
 set -e
 curl https://gist.githubusercontent.com/perillamint/481beca39ba82ff45dbe1b2dca86c673/raw/8cf20a067ba38e1e1cb9adf64f4bd28b440be3eb/tegra_mtc.bin.b64 | base64 -d > tegra_mtc.bin
 # You can verify this hash on https://fail0verflow.com/blog/2018/shofel2/
-echo "edb32e3f9ed15b55e780e8a01ef927a3b8a1f25b34a6f95467041d8953777d21 tegra_mtc.bin" | sha256sum -c -
+echo "edb32e3f9ed15b55e780e8a01ef927a3b8a1f25b34a6f95467041d8953777d21  tegra_mtc.bin" | sha256sum -c -
 set +e
 cd ..
