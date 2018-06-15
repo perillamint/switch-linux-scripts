e5eb41c63d9dd5a5df89f73879c441233c7c1139
perillamint <perillamint@quendi.moe>
2018-06-15 16:05:13 +0900

    Do not launch imx loader -- U-Boot loades kernel from sd card

--- load-docker.sh
+++ load-docker.sh
@@ -4,9 +4,3 @@ echo "Please put your Nintendo Switch in Tegra RCM mode."
 pushd shofel2/exploit
 ./shofel2.py ../../docker-result/cbfs.bin ../../docker-result/coreboot.rom
 popd
-
-# Wait for coreboot spins up...
-sleep 3
-
-cd usb_loader
-../imx_usb_loader/imx_usb -c .
