8e493815f90faed8fd62415b26678f132a46712c
perillamint <perillamint@quendi.moe>
2018-06-15 15:12:05 +0900

    Apply U-Boot patch

--- build.sh
+++ build.sh
@@ -30,5 +30,6 @@ export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 make nintendo-switch_defconfig
 cat .config  | sed 's#CONFIG_EXTRA_FIRMWARE=.*#CONFIG_EXTRA_FIRMWARE="nvidia/gm20b/pmu/desc.bin nvidia/gm20b/pmu/sig.bin nvidia/gm20b/pmu/image.bin nvidia/gm20b/acr/bl.bin nvidia/gm20b/acr/ucode_load.bin nvidia/gm20b/gr/sw_method_init.bin nvidia/gm20b/gr/gpccs_inst.bin nvidia/gm20b/gr/fecs_bl.bin nvidia/gm20b/gr/sw_bundle_init.bin nvidia/gm20b/gr/fecs_inst.bin nvidia/gm20b/gr/sw_ctx.bin nvidia/gm20b/gr/fecs_data.bin nvidia/gm20b/gr/gpccs_data.bin nvidia/gm20b/gr/sw_nonctx.bin nvidia/gm20b/gr/fecs_sig.bin nvidia/tegra210/xusb.bin nvidia/tegra210/vic04_ucode.bin"#g' > .config-new
+mv -f .config-new .config
 make -j$NPROC
 popd
